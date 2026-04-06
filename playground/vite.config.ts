6ae26eb2bb118489c4da195437a6fb1bf3af5f02
Ashrafkhan Azamov <88290250+aazamov@users.noreply.github.com>
2026-04-06 16:20:57 +0500

    fixed

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+  },
+  resolve: {
+    alias: {
+      "@ffmpeg/ffmpeg": path.resolve(__dirname, "node_modules/@ffmpeg/ffmpeg"),
+      "@ffmpeg/util": path.resolve(__dirname, "node_modules/@ffmpeg/util"),
+      pica: path.resolve(__dirname, "node_modules/pica"),
+    },
   },
   server: {
     headers: {
